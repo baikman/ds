239983b1e8187edd2c1029a72bf62c08a61b7a22
baikman <baikman@cedarville.edu>
2025-09-07 15:27:02 -0400

    hi hello just finished GCD class!

--- numeric/GCD.java
+++ numeric/GCD.java
@@ -1,7 +1,21 @@
 package numeric;
 
 public class GCD {
+    // Main function which handles I/O.
     public static void main(String[] args) {
+        System.out.println(computeGCD(Integer.parseInt(args[0]), Integer.parseInt(args[1])));
+    }
 
+    // computeGCD function, which returns the integer greatest common divisor of integers x and y, regardless of sign.
+    public static int computeGCD(int x, int y) {
+        if (y == 0) {
+            if (x > 0) {
+                return x;
+            } else {
+                return -x;
+            }
+        } else {
+            return computeGCD(y, x % y);
+        }
     }
 }
