d0c7cd63b4f38e89aa9bfe293141b3d0538a9caa
baikman <baikman@cedarville.edu>
2025-09-07 21:23:53 -0400

    added javadoc comments

--- numeric/Factorial.java
+++ numeric/Factorial.java
@@ -1,7 +1,22 @@
 package numeric;
 
+/**
+* This class computes the factorial of a non-negative integer.
+*
+* @author Brandon Aikman
+* @version 1.0
+* File: Factorial.java
+* Created: Sep 2025
+*
+* Description: This class computes the factorial of a non-negative integer iteratively.
+*/
+
 public class Factorial {
-    // Main function which handles I/O.
+    /**
+     * Main function which handles I/O from the command line.
+     *
+     * @param args one integer arguments to compute the factorial of.
+     */
     public static void main(String[] args) {
         if (args.length != 1) {
             System.out.println("Incorrect number of command-line parameters! Try again with just one.");
@@ -12,7 +27,12 @@ public class Factorial {
         }
     }
 
-    // computeFactorial function, which returns the double factorial of an integer argument x.
+    /**
+     * computeFactorial function, which returns the double factorial of an integer argument x.
+     *
+     * @param x integer argument to compute the factorial of.
+     * @return the factorial computed.
+     */
     public static double computeFactorial(int x) {
         double factorial = 1.0;
         if (x == 0 || x == 1) {
--- numeric/GCD.java
+++ numeric/GCD.java
@@ -1,7 +1,22 @@
 package numeric;
 
+/**
+* This class computes the greatest common divisor of two integers.
+*
+* @author Brandon Aikman
+* @version 1.0
+* File: GCD.java
+* Created: Sep 2025
+*
+* Description: This class computes the greatest common divisor of two integers using recursive function calls.
+*/
+
 public class GCD {
-    // Main function which handles I/O.
+    /**
+     * Main function which handles I/O from the command line.
+     *
+     * @param args two integer arguments to compute the GCD from.
+     */
     public static void main(String[] args) {
         if (args.length != 2) {
             System.out.println("Incorrect number of command-line parameters! Try again with two.");
@@ -9,8 +24,14 @@ public class GCD {
             System.out.println(computeGCD(Integer.parseInt(args[0]), Integer.parseInt(args[1])));
         }
     }
-
-    // computeGCD function, which returns the integer greatest common divisor of integers x and y, regardless of sign.
+    
+    /**
+     * computeGCD function, which returns the integer greatest common divisor.
+     *
+     * @param x first integer to compute GCD of.
+     * @param y second integer to compute GCD of.
+     * @return the result of the GCD function.
+     */
     public static int computeGCD(int x, int y) {
         if (y == 0) {
             if (x > 0) {
